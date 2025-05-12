213135a83065011bf67653cb43bb3616374e7a91
231098 <231098@CPD03-M16>
2025-05-12 15:54:26 -0300

    12/5

---
+++ MauiAppArvoredo/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiAppArvoredo.Models;
+using SQLite;
+
+namespace MauiAppArvoredo.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _connection;
+
+        public SQLiteDatabaseHelper(string path) 
+        {
+            _connection = new SQLiteAsyncConnection(path);
+            _connection.CreateTableAsync<Madeiras>().Wait();
+        }
+
+        public Task<int> Insert(Madeiras m) 
+        { 
+            return _connection.InsertAsync(m);
+        }
+
+        public Task<List<Madeiras>> Update(Madeiras m) 
+        {
+            string SQL = "UPDATE Madeiras SET Tipo=? WHERE id=?";
+
+            return _connection.QueryAsync<Madeiras>(SQL, m.Tipo);
+        }
+
+        public Task<int> Delete(int id) 
+        { 
+            return _connection.Table<Madeiras>().DeleteAsync(i => i.id == id);
+        }  
+
+        public void GetAll() { } 
+
+    }
+}
--- MauiAppArvoredo/Models/Madeiras.cs
+++ MauiAppArvoredo/Models/Madeiras.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace MauiAppArvoredo.Models
 {
     public class Madeiras
     {
-        public int? id { get; set; }
-        public string? tipo { get; set; }
+        string _tipo;
+
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string Tipo {
+            get => _tipo;
+            set
+            {
+                if (value == null)
+                    throw new Exception("Nome inválido");
+
+                _tipo = value;
+            }
+        }
+
     }
 }
